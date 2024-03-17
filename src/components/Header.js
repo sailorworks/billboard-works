89f2de582521cd6f0d3cd5894fb65158326b2272
Firstname Lastname <sahilpras11@gmail.com>
2024-03-17 19:53:11 +0530

    recent update in css

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,38 +1,107 @@
-// components/Header.js
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from 'react'; 
 
-function Header() {
-  return (
-    <header>
-      <div className="logo">
-        <Link to="/">Billboard Works</Link>
-      </div>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/about">About Us</Link>
-          </li>
-          <li>
-            <Link to="/services">Services</Link>
-          </li>
-          <li>
-            <Link to="/find-billboards">Find Billboards</Link>
-          </li>
-          <li>
-            <Link to="/sign-up-login">Sign Up/Log In</Link>
-          </li>
-        </ul>
-      </nav>
-      <div className="cta-buttons">
-        <button>Find Billboards</button>
-        <button>List Your Billboards</button>
-      </div>
-    </header>
-  );
-}
+import { Link } from 'react-router-dom'; 
 
-export default Header;
+import 'bulma/css/bulma.min.css'; 
+
+  
+
+function Header() { 
+
+  return ( 
+
+    <header className="has-background-white-ter"> 
+
+      <div className="container is-max-desktops pt-3 pb-3"> 
+
+        <div className="columns"> 
+
+          <div className="column is-two-thirds is-offset-one-quarter"> 
+
+            <nav className="level"> 
+
+              <div className="level-left mr-5"> 
+
+                <div className="level-item"> 
+
+                  <Link to="/" className="title is-size-4 has-text-black"> 
+
+                    Billboard Works 
+
+                  </Link> 
+
+                </div> 
+
+              </div> 
+
+              <div className="level-right ml-5"> 
+
+                <div className="level-item"> 
+
+                  <Link to="/" className="button is-rounded is-light is-outlined mr-1"> 
+
+                    HOME 
+
+                  </Link> 
+
+                </div> 
+
+                <div className="level-item"> 
+
+                  <Link to="/about" className="button is-rounded is-light is-outlined mr-1"> 
+
+                    ABOUT US 
+
+                  </Link> 
+
+                </div> 
+
+                <div className="level-item"> 
+
+                  <Link to="/services" className="button is-rounded is-light is-outlined mr-1"> 
+
+                    SERVICES 
+
+                  </Link> 
+
+                </div> 
+
+                <div className="level-item"> 
+
+                  <Link to="/find-billboards" className="button is-rounded is-success is-outlined mr-1"> 
+
+                    FIND BILLBOARDS 
+
+                  </Link> 
+
+                </div> 
+
+                <div className="level-item"> 
+
+                  <Link to="/sign-up-login" className="button is-rounded is-dark is-outlined"> 
+
+                    SIGN UP / LOG IN 
+
+                  </Link> 
+
+                </div> 
+
+              </div> 
+
+            </nav> 
+
+          </div> 
+
+        </div> 
+
+      </div> 
+
+    </header> 
+
+  ); 
+
+} 
+
+  
+
+export default Header; 
